410a22b0739fa372175c02d2d50f5581dc14165c
Frannx1 <fsanguineti@itba.edu.ar>
2017-05-31 08:37:19 -0300

    Add files via upload

--- Hackerman/src/model/entity/Entity.java
+++ Hackerman/src/model/entity/Entity.java
@@ -11,37 +11,6 @@ public abstract class Entity implements Serializable {
 
     public static final int IDLE = 0;
 
-    //private static final boolean PASSABLE;
-
     protected Position position;
-    protected Direction direction;
-
-    protected Entity(Position position, Direction direction) {
-        this.position = position;
-        this.direction = direction;
-    }
-
-    public Position getPosition() {
-        return position;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public void setPosition(Position position) {
-        this.position = position;
-    }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    boolean closeEnough(Entity entity, double delta) {
-        return this.position.isNearby(entity.getPosition(), delta);
-    }
-
-    abstract boolean isPassable();
-
 
 }
