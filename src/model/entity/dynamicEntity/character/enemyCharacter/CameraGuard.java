a4d8abcded7955ed032ff7cd9793db7fdfb40168
Frannx1 <fsanguineti@itba.edu.ar>
2017-06-04 17:14:56 -0300

    Add files via upload

--- src/model/entity/dynamicEntity/character/enemyCharacter/CameraGuard.java
+++ src/model/entity/dynamicEntity/character/enemyCharacter/CameraGuard.java
@@ -1,19 +1,81 @@
 package model.entity.dynamicEntity.character.enemyCharacter;
 
+import model.entity.Direction;
+import model.entity.Position;
+
+import java.util.List;
+
+import static model.entity.Entity.IDLE;
+
 /**
  * An Enemy that cant move
  */
 public class CameraGuard extends EnemyCharacter {
 
+    private List<Direction> instructions;
+    private int currentDirection;
+    private static final int NORMAL_ORIENTATION = 1;        //recorre en el sentido en que se guardan las direccions
+    private static final int INVERSE_ORIENTATION = -1;
 
-    /**
-     * Constructs a newly allocated {@code Character} object that
-     * represents the specified {@code char} value.
-     *
-     * @param value the value to be represented by the
-     *              {@code Character} object.
-     */
-    public CameraGuard(char value) {
-        super(value);
+
+    public CameraGuard(Position position, Direction direction, int range) {
+        super(position, direction, 0, range);
+        instructions = null;
+        currentDirection = 0;
     }
+
+    public CameraGuard(Position position, Direction direction, int range, List<Direction> instructions) {
+        super(position, direction, 0, range);
+        this. instructions = instructions;
+        currentDirection = 0;
+    }
+
+    public void addInstruction(Direction direction) {
+        instructions.add(direction);
+    }
+
+    public void addInstruction(Direction direction, int index) {
+        instructions.add(index, direction);
+    }
+
+    public void tick() {
+        if(instructions == null) {
+            return;
+        }
+        if(getState() == IDLE) {
+            rotate(nextDirection());
+        }
+        updateStatus();
+    }
+
+    public Direction nextDirection() {
+        if(isCycle()) {
+            updateCurrentDirection();
+        }
+        else {
+            updateOrientation();
+            updateCurrentDirection();
+        }
+        return instructions.get(currentDirection);
+    }
+
+    private void updateCurrentDirection() {
+        if(getPosition().equals(instructions.get(currentPosition)) {
+            currentPosition = (currentPosition + orientation) % instructions.size();
+        }
+    }
+
+    private void updateOrientation() {
+        if(getPosition.eguals(instructions.get(0))) {               //si estoy en la primer direccion
+            orientation = NORMAL_ORIENTATION;
+        }
+        else if(getPosition.eguals(instructions.get(0))) {          //si estoy en la ultima posision
+            orientation = INVERSE_ORIENTATION;
+        }
+    }
+
+    private boolean isCycle() {
+        return instructions.get(0).equals(instructions.get(instructions.size()));
+    }
+}
 }
--- src/model/entity/dynamicEntity/character/enemyCharacter/Guard.java
+++ src/model/entity/dynamicEntity/character/enemyCharacter/Guard.java
@@ -1,19 +1,85 @@
 package model.entity.dynamicEntity.character.enemyCharacter;
 
+import model.entity.Direction;
+import model.entity.Position;
+
+import java.util.List;
+
+import static model.entity.Entity.IDLE;
+
+
 /**
  * An Enemy that moves
  */
 public class Guard extends EnemyCharacter {
 
+    private List<Position> instructions;
+    private int currentPosition;
+    private static final int NORMAL_ORIENTATION = 1;        //recorre en el sentido en que se guardan las posiciones
+    private static final int INVERSE_ORIENTATION = -1;
+    private int orientation;
 
-    /**
-     * Constructs a newly allocated {@code Character} object that
-     * represents the specified {@code char} value.
-     *
-     * @param value the value to be represented by the
-     *              {@code Character} object.
-     */
-    public Guard(char value) {
-        super(value);
+    public Guard(Position position, Direction direction, int velocity, int range) {
+        super(position, direction, velocity, range);
+        instructions = null;
+        currentPosition = 0;
+    }
+
+    public Guard(Position position, Direction direction, int velocity, int range, List<Position> instructions) {
+        super(position, direction, velocity, range);
+        this. instructions = instructions;
+        currentPosition = 0;
+    }
+
+    public void addInstruction(Position position) {
+        instructions.add(position);
+    }
+
+    public void addInstruction(Position position, int index) {
+        instructions.add(index, position);
+    }
+
+    public void tick() {
+        if(instructions == null) {
+            return;
+        }
+        if(getState() == IDLE) {
+            Direction direction = nextDirection();
+            tryToMove(direction, grid);
+        }
+        move();
+        updateStatus();
+    }
+
+    public Direction nextDirection() {
+        if(isCycle()) {
+            updateCurrentPosition();
+        }
+        else {
+            updateOrientation();
+            updateCurrentPosition();
+        }
+        return Direction.directionBetween(getPosition(), instructions.get(currentPosition));
+
+    }
+
+    private void updateCurrentPosition() {
+        if(getPosition().equals(instructions.get(currentPosition)) {
+            currentPosition = (currentPosition + orientation) % instructions.size();
+        }
+    }
+
+    private void updateOrientation() {
+        if(getPosition.eguals(instructions.get(0))) {               //si estoy en la primer direccion
+            orientation = NORMAL_ORIENTATION;
+        }
+        else if(getPosition.eguals(instructions.get(0))) {          //si estoy en la ultima posision
+            orientation = INVERSE_ORIENTATION;
+        }
+    }
+
+    private boolean isCycle() {
+        return instructions.get(0).equals(instructions.get(instructions.size()));
     }
 }
+
