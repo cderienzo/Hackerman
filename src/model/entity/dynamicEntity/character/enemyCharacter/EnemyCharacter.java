68370da7a9dcd37fbbfd51946bd9a22355fb70d1
cderienzo <cderienzo@itba.edu.ar>
2017-06-04 13:33:13 -0300

    Add files via upload

--- src/model/entity/dynamicEntity/character/enemyCharacter/EnemyCharacter.java
+++ src/model/entity/dynamicEntity/character/enemyCharacter/EnemyCharacter.java
@@ -1,11 +1,27 @@
 package model.entity.dynamicEntity.character.enemyCharacter;
 
 /**
- * A character with a lattern
+ * A character with a lantern
  */
 public abstract class EnemyCharacter extends Character {
 
-    public EnemyCharacter(char value) {
-        super(value);
+    private Light mylight;
+
+    protected EnemyCharacter(Position position, Direction direction, int velocity, int range) {
+        super(position, direction, velocity);
+        mylight = new Light(range, position, direction);
+    }
+
+    public boolean hackerDetected(Grid grid) {
+       return mylight.collision(grid)
+    }
+    
+
+    public Light getMylight() {
+        return mylight;
+    }
+
+    public void setMylight(Light mylight) {
+        this.mylight = mylight;
     }
 }
