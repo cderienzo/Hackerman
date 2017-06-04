9c7f678f16f274704d96c3571d8ed77f74f6d1c3
cderienzo <cderienzo@itba.edu.ar>
2017-06-04 13:30:49 -0300

    Add files via upload

--- src/model/entity/dynamicEntity/DynamicEntity.java
+++ src/model/entity/dynamicEntity/DynamicEntity.java
@@ -33,6 +33,7 @@ public abstract class DynamicEntity extends Entity {
         timer = new Timer(velocity);
     }
 
+    
     public int getState() {
         return state;
     }
@@ -49,48 +50,11 @@ public abstract class DynamicEntity extends Entity {
         super.setDirection(direction);
     }
 
-    public void tryToMove(Direction direction, Grid grid) {
-        if (state != IDLE || direction == null)
-            return;
+    public abstract void tick();
 
-        if(!this.direction.equals(direction)) {
-            rotate(direction);
-            return;
-        }
+    public abstract void trytoMove(Direction direction, Grid grid);
 
-        int[] dir = direction.getDir();
-
-        Position destination = new Position(getPosition().getX() + dir[0], getPosition().getY() + dir[1]);
-
-        // Check destination is within the borders of the map, and its a valid
-        // destination.
-
-        if (!destination.withinBoundaries() || !grid.isPossibleAdd(destination))
-            return;
-        state = MOVING;
-        movesRemaining = Map.CELL_SIZE;
-        timer.updateLastMoveTime(System.currentTimeMillis(), direction);
-        grid.add(this);
-        grid.freePosition(this.getPosition());
-    }
-
-    protected void move() {
-        if (movesRemaining == 0) {
-            updateStatus();
-            return;
-        }
-
-        long nowTime = System.currentTimeMillis();
-
-        if (timer.moveTimePassed(nowTime)) {
-            timer.updateLastMoveTime(nowTime);
-
-            movesRemaining--;
-
-            int[] dir = direction.getDir();
-            getPosition().incrementPosition(dir[0], dir[1]);
-        }
-    }
+    public abstract void move();
 
     protected void updateStatus() {
         if (state == MOVING && movesRemaining <= 0)
