2b6900ecdfe3f54006eb612f8069caa522fda0d4
cderienzo <cderienzo@itba.edu.ar>
2017-06-05 13:55:23 -0300

    Add files via upload

---
+++ src/model/Managers/ComputerManager.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by cderienzo on 6/4/2017.
+ */
+public class ComputerManager {
+    private LinkedList<Computer> computers;
+    private Door myDoor;
+    private int currentComputer;
+
+    public ComputerManager(Door door, LinkedList<Computer> computers) {
+        if(computers != null && door != null){
+            this.computers = computers;
+            this.myDoor = door;
+            currentComputer = 0;
+            computers.get(currentComputer).setOn();
+        }
+    }
+
+    public void updateComputers() {
+        if(computers.get(currentComputer).isHacked()){
+            if(currentComputer == computers.size()) {
+                openDoor();
+            }
+            else{
+                currentComputer++;
+                computers.get(currentComputer).setOn();
+            }
+        }
+    }
+
+    public void openDoor() {
+        myDoor.setOpen();
+    }
+
+}
