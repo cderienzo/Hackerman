2d891440ec4bbcf94d4736d39ee3a77cc42b83b3
cderienzo <cderienzo@itba.edu.ar>
2017-06-05 13:54:48 -0300

    Add files via upload

---
+++ src/model/gameWorld/Level1.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+
+/**
+ * Created by cderienzo on 6/4/20WALL7.
+ */
+public class Level1 {
+
+    private final static int WIDTH = 13;
+    private final static int HEIGHT = 16;
+
+    public Level1(PlayerCharacter player) {
+        int[][] matrix = {
+                public enum obstacleType {FLOOR,DESK, WALL, FAKECOMPUTER};
+               //13X16
+                {WALL, WALL, WALL, WALL, WALL, WALL, DOOR, WALL, WALL, WALL, WALL, WALL, WALL},
+                {WALL,    0,    0,    0, 0, 0, 0, 0, 0, 0, 0, 0, WALL},
+                {WALL, WALL, WALL, WALL, WALL, 0, GUARD, 0, WALL, WALL, WALL, WALL, WALL},
+                {WALL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, DESK, WALL},
+                {WALL, 0, 0, 0, WALL, 0, 0, 0, WALL, 0, 0, 0, WALL},
+                {WALL, WALL, WALL, WALL, WALL, 0, 0, 0, WALL, WALL, WALL, WALL, WALL},
+                {WALL, WALL, WALL, WALL, WALL, 0, 0, 0, WALL, WALL, WALL, WALL, WALL},
+                {WALL, COMPUTER,DESK , 0, 0, WALL, 0, 0, 0, WALL, DESK, 0, 0, WALL},
+                {WALL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, WALL},
+                {WALL, WALL, WALL, WALL, WALL, 0, 0, 0, WALL, WALL, WALL, WALL, WALL},
+                {WALL, 0, 0, 0, WALL, 0, 0, 0, WALL, 0, 0, 0, WALL},
+                {WALL, DESK, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, WALL},
+                {WALL, WALL, WALL, WALL, WALL, 0, 0, 0, WALL, WALL, WALL, WALL, WALL},
+                {WALL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, WALL},
+                {WALL, 0, 0, 0, 0, 0, 0, 0, 0, PLAYER, 0, 0, WALL},
+                {WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL}
+        };
+        obstacleType state = obstacleType.FLOOR;
+        LinkedList<Obstacle> desks = new LinkedList<Obstacle>();
+        LinkedList<Obstacle> walls = new LinkedList<Obstacle>();
+        LinkedList<Obstacle> fakecomputers = new LinkedList<Obstacle>();
+        for(int i = 0; i<HEIGHT; i++) {
+            for (int j = 0; j<WIDTH; j++) {
+                switch(state){
+                    case DESK: desks.add(new Obstacle());
+                    case WALL: desks.add(new Obstacle());
+                    case FAKECOMPUTER: desks.add(new Obstacle());
+                }
+            }
+
+        }
+        EntityManager = new EntityManager();
+    }
+
+}
