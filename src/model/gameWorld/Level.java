d34239445c452505e885bb8470c8fcdd1d0f57da
sebasfavaron <sebas.favaron@hotmail.com>
2017-06-10 11:57:29 -0300

    Retoque Level y le agregue lo que faltaba

--- src/model/gameWorld/Level.java
+++ src/model/gameWorld/Level.java
@@ -16,6 +16,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -36,8 +37,6 @@ public class Level {
         try {
             byte[] bytes = Files.readAllBytes(f.toPath());
             return new String(bytes,"UTF-8");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,64 +47,126 @@ public class Level {
         //ej: guards[guards.lenght-1] no es un guard
         String s = readFile(filename);
         String[] guards = s.split(" GUARDS\n");
-        String[] cameraguards = guards[guards.length-1].split(" CAMERAGUARDS\n");
-        String[] computers = cameraguards[cameraguards.length-1].split(" COMPUTERS\n");
-        String[] doors = computers[computers.length-1].split(" DOORS\n");
-        String[] desks = doors[doors.length-1].split(" DESKS\n");
-        String[] hackers = desks[desks.length-1].split(" HACKER\n");
-        String[] maps = hackers[hackers.length-1].split(" MAP");
+        String[] cameraguards = guards[guards.length - 1].split(" CAMERAGUARDS\n");
+        String[] computers = cameraguards[cameraguards.length - 1].split(" COMPUTERS\n");
+        String[] doors = computers[computers.length - 1].split(" DOORS\n");
+        String[] desks = doors[doors.length - 1].split(" DESKS\n");
+        String[] hackers = desks[desks.length - 1].split(" HACKER\n");
+        String[] maps = hackers[hackers.length - 1].split(" MAP");
         String map = maps[0];
-        String[] mapRows = map.split("/");
+        String[] mapRows = map.split("/\n");
         LinkedList<LinkedList<Entity>> grid = new LinkedList<>();
-        int guardIndex = 0,cameraIndex = 0,computerIndex = 0,doorIndex = 0,deskIndex = 0;
+        int guardIndex = 0, cameraIndex = 0, computerIndex = 0, doorIndex = 0, deskIndex = 0;
         int rowNumber = 0;
-        for(String row:mapRows){
+        for (String row : mapRows) {
             LinkedList<Entity> aRow = new LinkedList<>();
-            grid.add(rowNumber,aRow);
-            System.out.println(rowNumber+": "+row);
+            grid.add(rowNumber, aRow);
+            System.out.println(rowNumber + ": " + row);
             String[] cells = row.split(",");
             int cellNumber = 0;
-            for(String cell:cells){
-                Position position = new Position(rowNumber,cellNumber);
-                if(cell.equals("WALL")){
-                    Direction direction = new Direction(0);
-                    grid.get(rowNumber).add(cellNumber,new Obstacle(position,direction,Obstacle.obstacleType.WALL));
+            for (String cell : cells) {
+                Position position = new Position(rowNumber, cellNumber);
+                switch (cell){
+                    case "WALL":
+                        Direction direction = new Direction(0);
+                        grid.get(rowNumber).add(cellNumber, new Obstacle(position, direction, Obstacle.obstacleType.WALL));
+                        break;
+                    case "DOOR":
+                        String[] properties = doors[doorIndex++].split(",");
+                        direction = new Direction(Integer.valueOf(properties[0]));
+                        grid.get(rowNumber).add(cellNumber, new Door(position, direction));
+                        break;
+                    case "GUARD":
+                        properties = guards[guardIndex++].split(",");
+                        direction = new Direction(Integer.valueOf(properties[0]));
+                        int velocity = Integer.valueOf(properties[1]);
+                        int range = Integer.valueOf(properties[2]);
+                        if(properties.length>3){
+                            ArrayList<Position> instructions = new ArrayList<>();
+                            for(int i=3;i<properties.length;i+=2){
+                                instructions.add(new Position(i,i+1));
+                            }
+                            grid.get(rowNumber).add(cellNumber, new Guard(position, direction, velocity, range,instructions));
+                        }
+                        else {
+                            grid.get(rowNumber).add(cellNumber, new Guard(position, direction, velocity, range));
+                        }
+                        break;
+                    case "CAMERAGUARD":
+                        properties = cameraguards[cameraIndex++].split(",");
+                        direction = new Direction(Integer.valueOf(properties[0]));
+                        range = Integer.valueOf(properties[1]);
+                        if(properties.length>2){
+                            ArrayList<Direction> instructions = new ArrayList<>();
+                            for(int i=2;i<properties.length;i++){
+                                instructions.add(new Direction(i));
+                            }
+                            grid.get(rowNumber).add(cellNumber, new CameraGuard(position, direction, range,instructions));
+                        }
+                        else {
+                            grid.get(rowNumber).add(cellNumber, new CameraGuard(position, direction, range));
+                        }
+                        break;
+                    case "HACKER":
+                        properties = hackers[0].split(",");
+                        direction = new Direction(Integer.valueOf(properties[0]));
+                        velocity = Integer.valueOf(properties[1]);
+                        grid.get(rowNumber).add(cellNumber, new PlayerCharacter(position, direction, velocity));
+                        break;
+                    case "COMPUTER":
+                        properties = computers[computerIndex].split(",");
+                        direction = new Direction(Integer.valueOf(properties[0]));
+                        int consecutiveHacks = Integer.valueOf(properties[1]);
+                        grid.get(rowNumber).add(cellNumber, new Computer(position, direction, consecutiveHacks));
+                        break;
+                    case "DESK":
+                        properties = desks[deskIndex].split(",");
+                        direction = new Direction(Integer.valueOf(properties[0]));
+                        grid.get(rowNumber).add(cellNumber, new Obstacle(position, direction, Obstacle.obstacleType.DESK));
+                        break;
                 }
-                else if(cell.equals("DOOR")){
+                /*if (cell.equals("WALL")) {
+                    Direction direction = new Direction(0);
+                    grid.get(rowNumber).add(cellNumber, new Obstacle(position, direction, Obstacle.obstacleType.WALL));
+                } else if (cell.equals("DOOR")) {
                     String[] properties = doors[doorIndex++].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
-                    grid.get(rowNumber).add(cellNumber,new Door(position,direction));
-                }
-                else if(cell.equals("GUARD")){
+                    grid.get(rowNumber).add(cellNumber, new Door(position, direction));
+                } else if (cell.equals("GUARD")) {
                     String[] properties = guards[guardIndex++].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
-                    int velocity=Integer.valueOf(properties[1]);
-                    int range=Integer.valueOf(properties[2]);
-                    grid.get(rowNumber).add(cellNumber,new Guard(position,direction,velocity,range));
-                }
-                else if(cell.equals("CAMERAGUARD")){
+                    int velocity = Integer.valueOf(properties[1]);
+                    int range = Integer.valueOf(properties[2]);
+                    if(properties.length>3){
+                        ArrayList<Position> instructions = new ArrayList<>();
+                        for(int i=3;i<properties.length;i+=2){
+                            instructions.add(new Position(i,i+1));
+                        }
+                        grid.get(rowNumber).add(cellNumber, new Guard(position, direction, velocity, range,instructions));
+                    }
+                    else {
+                        grid.get(rowNumber).add(cellNumber, new Guard(position, direction, velocity, range));
+                    }
+                } else if (cell.equals("CAMERAGUARD")) {
                     String[] properties = cameraguards[cameraIndex++].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
-                    int range=Integer.valueOf(properties[1]);
-                    grid.get(rowNumber).add(cellNumber,new CameraGuard(position,direction,range));
-                }
-                else if(cell.equals("HACKER")){
+                    int range = Integer.valueOf(properties[1]);
+                    grid.get(rowNumber).add(cellNumber, new CameraGuard(position, direction, range));
+                } else if (cell.equals("HACKER")) {
                     String[] properties = hackers[0].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
-                    int velocity=Integer.valueOf(properties[1]);
-                    grid.get(rowNumber).add(cellNumber,new PlayerCharacter(position,direction,velocity));
-                }
-                else if(cell.equals("COMPUTER")){
+                    int velocity = Integer.valueOf(properties[1]);
+                    grid.get(rowNumber).add(cellNumber, new PlayerCharacter(position, direction, velocity));
+                } else if (cell.equals("COMPUTER")) {
                     String[] properties = computers[computerIndex].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
-                    int consecutiveHacks=Integer.valueOf(properties[1]);
-                    grid.get(rowNumber).add(cellNumber,new Computer(position,direction,consecutiveHacks));
-                }
-                else if(cell.equals("DESK")){
+                    int consecutiveHacks = Integer.valueOf(properties[1]);
+                    grid.get(rowNumber).add(cellNumber, new Computer(position, direction, consecutiveHacks));
+                } else if (cell.equals("DESK")) {
                     String[] properties = desks[deskIndex].split(",");
                     Direction direction = new Direction(Integer.valueOf(properties[0]));
-                    grid.get(rowNumber).add(cellNumber,new Obstacle(position,direction,Obstacle.obstacleType.DESK));
-                }
+                    grid.get(rowNumber).add(cellNumber, new Obstacle(position, direction, Obstacle.obstacleType.DESK));
+                }*/
                 cellNumber++;
             }
             rowNumber++;
